bd58a9cc6d715577e91da75274e3e1f0117fce23
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-07-12 20:16:43 -0300

    refactor: start over

--- portfolio/src/pages/_document.tsx
+++ portfolio/src/pages/_document.tsx
@@ -11,6 +11,9 @@ export default class MyDocument extends Document {
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
 
+          {/* bootstrap 5 icons */}
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+
           {/* google fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -1,15 +1,7 @@
-import PersonalDetails from "../components/PersonalDetails"
-
-
-export default function Home() {
+export default function Index() {
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-12 col-lg-4">
-          <PersonalDetails />
-        </div>
-      </div>
-      <h1>Hello world</h1>
+
     </div>
   )
 }
