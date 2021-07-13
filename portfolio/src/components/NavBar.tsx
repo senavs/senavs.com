4fd92b8bdc98876ad6642bf1aeef0969b8f28682
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-07-12 21:18:36 -0300

    feat: create navbar

---
+++ portfolio/src/components/NavBar.tsx
@@ -0,0 +1,52 @@
+interface NavBarProps {
+  aboutId: string,
+  resumeId: string,
+  experienceId: string,
+  educationId: string,
+  certificationsId: string
+  contactMe: string
+}
+
+export default function NavBar({ aboutId, resumeId, experienceId, educationId, certificationsId, contactMe }: NavBarProps) {
+  return (
+    <ul className="nav nav-pills my-3 d-flex justify-content-evenly" role="tablist">
+
+      <li className="nav-item" role="presentation">
+        <button className="nav-link active" data-bs-toggle="tab" data-bs-target={`#${aboutId}`} type="button" role="tab" aria-selected="true">
+          <i className="bi bi-person-fill me-2"></i><span>About me</span>
+        </button>
+      </li>
+
+      <li className="nav-item" role="presentation">
+        <button className="nav-link" data-bs-toggle="tab" data-bs-target={`#${resumeId}`} type="button" role="tab" aria-selected="false">
+          <i className="bi bi-cpu-fill me-2"></i><span>Resume</span>
+        </button>
+      </li>
+
+      <li className="nav-item" role="presentation">
+        <button className="nav-link" data-bs-toggle="tab" data-bs-target={`#${experienceId}`} type="button" role="tab" aria-selected="false">
+          <i className="bi bi-briefcase-fill me-2"></i><span>Experience</span>
+        </button>
+      </li>
+
+      <li className="nav-item" role="presentation">
+        <button className="nav-link" data-bs-toggle="tab" data-bs-target={`#${educationId}`} type="button" role="tab" aria-selected="false">
+          <i className="bi bi-journal-bookmark-fill me-2"></i><span>Education</span>
+        </button>
+      </li>
+
+      <li className="nav-item" role="presentation">
+        <button className="nav-link" data-bs-toggle="tab" data-bs-target={`#${certificationsId}`} type="button" role="tab" aria-selected="false">
+          <i className="bi bi-award-fill me-2"></i><span>Certification</span>
+        </button>
+      </li>
+
+      <li className="nav-item" role="presentation">
+        <button className="nav-link" data-bs-toggle="tab" data-bs-target={`#${contactMe}`} type="button" role="tab" aria-selected="false">
+          <i className="bi bi-telephone-fill me-2"></i><span>Contact Me</span>
+        </button>
+      </li>
+
+    </ul>
+  )
+}
--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -1,20 +1,40 @@
+import NavBar from "../components/NavBar";
 import PersonalInfo from "../components/PersonalInfo";
 
 export default function Index() {
   return (
     <div className="container">
 
-      <div className="row">
+      {/* nav bar */}
+      <nav className="text-center">
+        <NavBar
+          aboutId="about"
+          resumeId="resume"
+          experienceId="experience"
+          educationId="education"
+          certificationsId="certification"
+          contactMe="contact"
+        />
+      </nav>
 
+      {/* content */}
+      <main className="row">
+
+        {/* personal information */}
         <div className="col-md-4 col-12">
           <PersonalInfo />
         </div>
 
-        <div className="col-md-8 col-12">
+        {/* sections */}
+        <section className="col-md-8 col-12">
+          <div className="tab-content" id="myTabContent">
+            <div className="tab-pane fade show active" id="about" role="tabpanel" aria-labelledby="home-tab">about</div>
+            <div className="tab-pane fade" id="resume" role="tabpanel" aria-labelledby="profile-tab">resume</div>
+            <div className="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">contact</div>
+          </div>
+        </section>
 
-        </div>
-
-      </div>
+      </main>
     </div>
   )
 }
