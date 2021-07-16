e96aeb70804a7b6ea2e93302f801da98f8ffea41
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-07-15 22:06:16 -0300

    feat: add achievements section with volunteer experience

--- portfolio/src/components/achievements/AchievementsSection.tsx
+++ portfolio/src/components/achievements/AchievementsSection.tsx
@@ -3,9 +3,8 @@ export default function AchievementsSection() {
   return (
     <div className="accordion">
 
-      {/* about me */}
+      {/* certifications */}
       <div className="accordion-item">
-
         <div id="about-section" className="accordion-collapse collapse show" >
           <div className="accordion-body">
             <div className="row">
@@ -19,6 +18,34 @@ export default function AchievementsSection() {
                 <div className="ms-5">
                   <i className="bi bi-star-fill me-3"></i>
                   <span>Stefanini - Hackathon of Artificial Intelligence and Big Data</span>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* volunteer */}
+      <div className="accordion-item">
+        <div id="about-section" className="accordion-collapse collapse show" >
+          <div className="accordion-body">
+            <div className="row">
+              <div className="col-12">
+                <h2>Volunteer experience</h2>
+
+                <div className="ms-5">
+                  <i className="bi bi bi-tools me-3"></i>
+                  <span>UPIS - Algorithms Tutor</span>
+                </div>
+                <div className="ms-5">
+                  <i className="bi bi bi-tools me-3"></i>
+                  <span>UPIS - Mathematics Tutor</span>
+                </div>
+                <div className="ms-5">
+                  <i className="bi bi bi-tools me-3"></i>
+                  <span>UPIS - Laboratory Refactoring</span>
+
                 </div>
               </div>
             </div>
